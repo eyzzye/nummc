fbc4d6c62b0f93e34a3ac8bc6a80a69adeabb85b
eyzzye <eyuguchi@hotmail.com>
2021-03-08 19:34:12 +0900

    modify version 1.04.000

--- nummc/src/game_common.h
+++ nummc/src/game_common.h
@@ -4,7 +4,7 @@
 #include "SDL_mixer.h"
 #include "SDL_ttf.h"
 
-#define GAME_VERSION "nummc ver.1.03.000\n"
+#define GAME_VERSION "nummc ver.1.04.000\n"
 
 #define _COLLISION_ENABLE_BOX_2D_
 //#define _GAME_TIMER_FPS_ENABLE
